21f6bfbdb19f8ebac4e8107cde11e576c163fe77
Salus101 <k.mariasalus@gmail.com>
2023-03-04 15:24:28 +0300

    Inline example 2 for Javascript Insertion

---
+++ Week6/index.js
@@ -0,0 +1,10 @@
+// var firstName ="John";
+// var lastname = "Doe";
+
+// console.log("Full name: " + firstName + " " +lastname)
+
+var first = 7;
+var second = 5;
+
+console.log('The total is ' + first + second);
+console.log(first + second + ' is the total');
